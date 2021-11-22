cdb8dac208e66a3b910a947865fccdf00b635dd5
Juan Alemán <87498069+Alemango@users.noreply.github.com>
2021-11-22 14:09:11 -0600

    Español
    Variables

--- Asistente/try1.py
+++ Asistente/try1.py
@@ -6,16 +6,16 @@ def from_mic():
     speech_config = speechsdk.SpeechConfig(subscription="2b8110f589a14d9e8636dda17cce99e9", region="southcentralus")
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="es-MX")
     
-    print("Speak into your microphone.")
-    result = speech_recognizer.recognize_once_async().get()
+    print("Di algo.")
+    resultado = speech_recognizer.recognize_once_async().get()
 
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(result.text))
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if resultado.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Dijiste: {}".format(resultado.text))
+    elif resultado.reason == speechsdk.ResultReason.NoMatch:
+        print("No pudo ser reconocido: {}".format(resultado.no_match_details))
+    elif resultado.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = resultado.cancellation_details
+        print("El reconocimiento fue cancelado: {}".format(cancellation_details.reason))
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             print("Error details: {}".format(cancellation_details.error_details))
 
