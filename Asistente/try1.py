733b250c73e761337976eb09347da106a882f621
Juan Alemán <87498069+Alemango@users.noreply.github.com>
2021-11-22 14:17:16 -0600

    Documentación
    Comentarios

--- Asistente/try1.py
+++ Asistente/try1.py
@@ -1,25 +1,25 @@
-import azure.cognitiveservices.speech as speechsdk
+import azure.cognitiveservices.speech as speechsdk #Importamos la librería de S2T de Azure
 
-hola = "Hola."
+hola = "Hola." #Variable de control
 
 def from_mic():
-    speech_config = speechsdk.SpeechConfig(subscription="2b8110f589a14d9e8636dda17cce99e9", region="southcentralus")
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="es-MX")
+    speech_config = speechsdk.SpeechConfig(subscription="2b8110f589a14d9e8636dda17cce99e9", region="southcentralus") #Suscripción y región no activa, requiere autorización
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language="es-MX") #Configuramos para español de México
     
     print("Di algo.")
-    resultado = speech_recognizer.recognize_once_async().get()
+    resultado = speech_recognizer.recognize_once_async().get() #Toma de datos por voz
 
-    if resultado.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Dijiste: {}".format(resultado.text))
-    elif resultado.reason == speechsdk.ResultReason.NoMatch:
-        print("No pudo ser reconocido: {}".format(resultado.no_match_details))
-    elif resultado.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = resultado.cancellation_details
-        print("El reconocimiento fue cancelado: {}".format(cancellation_details.reason))
+    if resultado.reason == speechsdk.ResultReason.RecognizedSpeech: #Si se reconoce
+        print("Dijiste: {}".format(resultado.text))                 #imprimelo
+    elif resultado.reason == speechsdk.ResultReason.NoMatch:                    #Si no se reconoce
+        print("No pudo ser reconocido: {}".format(resultado.no_match_details))  #imprime los detalles de fallo  
+    elif resultado.reason == speechsdk.ResultReason.Canceled:                               #Si se cancela el reconocimiento
+        cancellation_details = resultado.cancellation_details                               #ponlo en una variable
+        print("El reconocimiento fue cancelado: {}".format(cancellation_details.reason))    #imprime la razón de la cancelación
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
+            print("Error details: {}".format(cancellation_details.error_details))           #imprime los detalles del error de la cancelación
 
-    if result.text == hola:
+    if result.text == hola:     #Comparalo con la variable de control
         print("Hecho")
 
-from_mic()
+from_mic() #Inicia la funcion
