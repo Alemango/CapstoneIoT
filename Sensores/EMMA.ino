80413f5d444b89364f84e26fb04866bfad773f80
Juan Alemán <juanaleman19@gmail.com>
2022-02-23 15:04:26 -0600

    Comentarios micro
    Se documentan los códigos utilizados en las lecturas del microcontrolador

--- Sensores/EMMA.ino
+++ Sensores/EMMA.ino
@@ -1,137 +1,182 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include "ThingSpeak.h"
-#include "SensorMAX.h"
-#include "DHT.h"
+/*
+  Diplomado Internet de las cosas | Samsung Innovation Campus
 
-#define pin1 13
+  Proyecto Capstone: Asistente Médico
+  Alumno: Juan Jesús Alemán Espriella
 
-const char* ssid = "ESPRIELLAS";  // Aquí debes poner el nombre de tu red
-const char* password = "?uU:FE%P6s9d9b";  // Aquí debes poner la contraseña de tu red
+  Dispositivo: ESP32 (38 Pines)
+
+  Descripción: Código que implementa los sensores MAX30100, DHT11 y MQ-135 para la obtención de BPM, SPO2 en un paciente, Temperatura, Humedad
+              y calidad de aire en una habitación. Esta información se enviará a un servidor de ThingSpeak donde se almacenará y analizará la
+              información. Además de enviar un mensaje MQTT a través del broker de HiveMQ que nos servirá para mantener actualizada la información
+              en vivo desde la aplicación móvil.
+*/
+
+#include <WiFi.h> //Biblioteca para la conexión WiFi.
+#include <PubSubClient.h> //Biblioteca para la conexión MQTT. Autor: Nick O'Leary
+#include "ThingSpeak.h" //Biblioteca para la comunicación con ThingSpeak. Autor: MathWorks
+#include "SensorMAX.h" //Archivo para el funcionamiento del sensor MAX30100
+#include "DHT.h" //Biblioteca para el funcionamiento del sensor DHT11. Autor: Adafruit
+
+#define pin1 13 //Definición del pin usado para el sensor DHT11
+#define MQ135_THRESHOLD_1   1000 //Definición del límite de partículas para el sensor MQ-135
+
+const char* ssid = "ESPRIELLAS";  //Nombre de red
+const char* password = "?uU:FE%P6s9d9b";  //Contraseña de red
 
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
 PubSubClient client(espClient); // Este objeto maneja los datos de conexion al broker
-DHT dht1(pin1, DHT11);    //El azul.
+DHT dht1(pin1, DHT11);    //Objeto para el sensor DHT
 
-IPAddress server(18,198,247,0);
+IPAddress server(18,198,247,0); //Dirección IP del broker MQTT
 
-unsigned long channelID = 1648132;
-const char* WriteAPIKey = "3BZO9QOJ5C6OBWF1";
+unsigned long channelID = 1648132; //Canal de ThingSpeak
+const char* WriteAPIKey = "3BZO9QOJ5C6OBWF1"; //Clave de escritura para la API de Thingspeak
 
+/*
+  Función SetUp para el microcontrolador. Se realizan las instrucciones que sólo se ejecutarán una vez, como son: Conexión WiFi, e inicio de sensores.
+*/
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //Se inicializa la conexión a 115200 baudios
   
+  //Se imprime el intento de conexión WiFi
   Serial.println();
   Serial.println();
   Serial.print("Conectar a ");
-  Serial.println(ssid);
+  Serial.println(ssid); 
 
   WiFi.begin(ssid, password); // Esta es la función que realiza la conexión a WiFi
 
+  //Función que reintenta la conexión WiFi
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(100);
   }
 
+  //Se imprime la conexión a WiFi y la IP asignada
   Serial.println();
   Serial.println("WiFi conectado");
   Serial.println("Direccion IP: ");
   Serial.println(WiFi.localIP());
 
-  delay(5000);
+  delay(5000); //Retraso de 5 segundos
 
-  client.setServer(server,1883);
-  ThingSpeak.begin(espClient);
+  client.setServer(server,1883); //Se realiza la conexión MQTT
+  ThingSpeak.begin(espClient); //Se realiza la conexión con ThingSpeak
 
- setMAX();
- DHTsetup();
+ setMAX(); //Se inicia el sensor MAX30100
+ DHTsetup(); //Se inicia el sensor DHT
 }
 
+/*
+  Función reconnect que intenta y reintenta la conexión MQTT.
+*/
 void reconnect() {
-  // Loop until we're reconnected
+  // Loop hasta que se conecte
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+    // Intento de conexión
     if (client.connect("espClient")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
+      // Una vez conectado publica...
       client.publish("emma/prototipo/-Mryro7TSaIlYV7cMJns","nuevo");
-      // ... and resubscribe
+      // ...y se suscribe
       client.subscribe("emma/prototipo/-Mryro7TSaIlYV7cMJns");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
+      // Espera 5 segundos para la siguiente conexión
       delay(5000);
     }
   }
 }
 
+/*
+  Función loop que repite una serie de instrucciones mientras el microcontrolador esta encendido. Se incluye la lectura del sensor y el envío de la 
+  información MQTT y a ThingSpeak.
+*/
 void loop() {
 
+  //Conexión y reconexión MQTT
   if(!client.connected()){
     reconnect();
   }
-client.loop();
+client.loop(); //Loop que mantiene viva la conexión con el broker MQTT
 
- MAXloop();
- leerdht1();
- MQloop();
+ MAXloop(); //Lectura y envíos del sensor MAX30100
+ leerdht1(); //Lectura y envíos del sensor DHT
+ MQloop(); //Lectura y envíos del sensor MQ135
 
- ThingSpeak.writeFields(channelID,WriteAPIKey);
- Serial.println("Datos enviados a ThingSpeak!");
- delay(20000);
+ ThingSpeak.writeFields(channelID,WriteAPIKey); //Datos para la escritura de información en ThingSpeak
+ Serial.println("Datos enviados a ThingSpeak!"); 
+ delay(20000); //Espera de 20 segundos, debido a que ThingSpeak tiene un tiempo de aceptación de datos de 14 segundos.
 }
 
+/*
+  Función setup para el sensor Dht
+*/
 void DHTsetup() {
   
   Serial.begin(115200);
   Serial.println("Test de sensores:");
 
-  dht1.begin();
+  dht1.begin(); //Se inicia el sensor DHT
   
 }
 
+/*
+  Función de lectura y envío de datos para el sensor DHT
+*/
 void leerdht1() {
   
+  //Creamos 2 variables de tipo float para el almacenamiento de la temperatura y humedad
   float t1 = dht1.readTemperature();
   float h1 = dht1.readHumidity();
 
+  //Bucle para obtener lecturas válidas del sensor
   while (isnan(t1) || isnan(h1)){
     Serial.println("Lectura fallida en el sensor DHT11, repitiendo lectura...");
     delay(2000);
-    t1 = dht1.readTemperature();
-    h1 = dht1.readHumidity();
+    t1 = dht1.readTemperature(); //Se almacena el valor de temperatura
+    h1 = dht1.readHumidity(); //Se almacena el valor de humedad
   }
 
+  //Se imprime el valor en °C
   Serial.print("Temperatura DHT11: ");
   Serial.print(t1);
   Serial.println(" ºC.");
 
+  //Se imprime el % de humedad
   Serial.print("Humedad DHT11: ");
   Serial.print(h1);
   Serial.println(" %."); 
 
   Serial.println("-----------------------");
 
+  //Se escriben envían y almacenan los datos en ThingSpeak
   ThingSpeak.setField (3,t1);
   ThingSpeak.setField (4,h1);
 }
 
+/*
+  Función para la lectura del sensor MQ135
+*/
 void MQloop() {
-  
+
+   //Se inicia una variable para la lectura análoga del sensor MQ135 
    int MQ135_data = analogRead(A6);
 
+  //Se compara la lectura obtenida con el límite definido desde el inicio
   if(MQ135_data < MQ135_THRESHOLD_1){
     Serial.print("Buena Calidad de Aire: ");
   } else {
     Serial.print("Mala Calidad de Aire: "); 
   }
-    Serial.print(MQ135_data); // analog data
-    Serial.println(" PPM"); // Unit = part per million
+    Serial.print(MQ135_data); // Datos obtenidos
+    Serial.println(" PPM"); //Partes por millón
   
   Serial.println("*****************************************************");
 
-  ThingSpeak.setField (5,MQ135_data);
+  ThingSpeak.setField (5,MQ135_data); // Se envía el dato a ThingSpeak
 }
--- Sensores/SensorMAX.h
+++ Sensores/SensorMAX.h
@@ -1,16 +1,27 @@
-#include <Wire.h>
-#include "MAX30105.h"
-#include "spo2_algorithm.h"
-#include "ThingSpeak.h"
+/*
+  Diplomado Internet de las cosas | Samsung Innovation Campus
 
-unsigned long chanelID = 1648132;
-const char* WriteKey = "3BZO9QOJ5C6OBWF1";
+  Proyecto Capstone: Asistente Médico
+  Alumno: Juan Jesús Alemán Espriella
 
-MAX30105 particleSensor;
+  Dispositivo: ESP32 (38 Pines)
 
-#define MAX_BRIGHTNESS 255
+  Descripción: Código para la inicialización y lectura del sensor MAX30100. Así como el envío de datos a ThingSpeak.
+*/
 
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+#include <Wire.h> //Biblioteca que nos permite la creación de un objeto Wire.
+#include "MAX30105.h" //Biblioteca que nos permite el manejo del sensor MAX.
+#include "spo2_algorithm.h" //Biblioteca para la obtención del SPO2 con el sensor MAX.
+#include "ThingSpeak.h" //Biblioteca para la comunicación con ThingSpeak. Autor: MathWorks
+
+unsigned long chanelID = 1648132; //Canal de ThingSpeak
+const char* WriteKey = "3BZO9QOJ5C6OBWF1"; //Clave de escritura para la API de Thingspeak
+
+MAX30105 particleSensor; //Creación del objeto MAX
+
+#define MAX_BRIGHTNESS 255 //Definición del brillo en el led del sensor. Influye en la corriente utilizada.
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__) //Especificaciones para el chip ATMega
 
 uint16_t irBuffer[100]; //infrared LED sensor data
 uint16_t redBuffer[100];  //red LED sensor data
@@ -19,21 +30,28 @@ uint32_t irBuffer[100]; //infrared LED sensor data
 uint32_t redBuffer[100];  //red LED sensor data
 #endif
 
-int32_t bufferLength; //data length
-int32_t spo2; //SPO2 value
-int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
-int32_t heartRate; //heart rate value
-int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+int32_t bufferLength; //Tamaño del dato
+int32_t spo2; //valor SPO2
+int8_t validSPO2; //Indicador para la lectura válida de SPO2
+int32_t heartRate; //Valor del BPM
+int8_t validHeartRate; //Indicador para la lectura válida de BPM
 
-byte pulseLED = 12; //Must be on PWM pin
-byte readLED = 13; //Blinks with each data read
+byte pulseLED = 12; //Debe ser un pin PWM
+byte readLED = 13; //Parpadea con cada lectura 
 
+/*
+  Función setup 
+*/
 void setMAX(){
+  //Definición de pines a utilizar
   pinMode(pulseLED, OUTPUT);
   pinMode(readLED, OUTPUT);
 
+  //Creamos el objeto Wire
    Wire.begin (14,15);
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+
+  //Verificamos la conexión del sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Usamos el puerto I2C por default, 400kHz 
   {
     Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
@@ -42,25 +60,26 @@ void setMAX(){
   Serial.println(F("Attach sensor to finger with rubber band. Readings Will Start after 5 seconds"));
   delay (5000);
   
-  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 411; //Options: 69, 118, 215, 411
-  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  byte ledBrightness = 60; //Opciones: 0=Off to 255=50mA
+  byte sampleAverage = 32; //Opciones: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Opciones: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Opciones: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Opciones: 69, 118, 215, 411
+  int adcRange = 16384; //Opciones: 2048, 4096, 8192, 16384
 
+  //Iniciamos el sensor
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 
    bufferLength = 100;
 
    for (byte i = 0 ; i < bufferLength ; i++)
   {
-    while (particleSensor.available() == false) //do we have new data?
-      particleSensor.check(); //Check the sensor for new data
+    while (particleSensor.available() == false) //Verificamos si hay nuevos datos
+      particleSensor.check(); //Checamos los datos
 
     redBuffer[i] = particleSensor.getRed();
     irBuffer[i] = particleSensor.getIR();
-    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    particleSensor.nextSample(); 
 
     Serial.print(F("red="));
     Serial.print(redBuffer[i], DEC);
@@ -71,6 +90,9 @@ void setMAX(){
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 }
 
+/*
+  Función loop para el sensor MAX
+*/
 void MAXloop(){
   for (byte i = 25; i < 100; i++)
     {
@@ -78,17 +100,16 @@ void MAXloop(){
       irBuffer[i - 25] = irBuffer[i];
     }
 
-    //take 25 sets of samples before calculating the heart rate.
+    //Toma 25 samples antes de dar el BPM
     for (byte i = 75; i < 100; i++)
     {
-      while (particleSensor.available() == false) //do we have new data?
-        particleSensor.check(); //Check the sensor for new data
-
-      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+      while (particleSensor.available() == false) //Nuevos datos?
+        particleSensor.check(); //Checamos por nuevos datos
+      digitalWrite(readLED, !digitalRead(readLED)); //Parpadea para cada dato nuevo
 
       redBuffer[i] = particleSensor.getRed();
       irBuffer[i] = particleSensor.getIR();
-      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+      particleSensor.nextSample(); //Se finaliza
 
       Serial.print(F("HR="));
       Serial.print(heartRate, DEC);
@@ -96,15 +117,14 @@ void MAXloop(){
       Serial.print(F(", SPO2="));
       Serial.println(spo2, DEC);
 
-        ThingSpeak.setField (1,heartRate);
-        ThingSpeak.setField (2,spo2);
+      //Envío de datos a ThingSpeak
+      ThingSpeak.setField (1,heartRate); 
+      ThingSpeak.setField (2,spo2);
     }
 
-    //After gathering 25 new samples recalculate HR and SP02
+    //Después de 25 muestras, recalcula el BPM y SPO2
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
- ThingSpeak.writeFields(chanelID,WriteKey);
-
-    //Fin de codigo del lectura del sensor presente en el ejemplo
+  ThingSpeak.writeFields(chanelID,WriteKey);
   //Fin de lectura del sensor  
 }
